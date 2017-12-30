a64b7b632d21bdac2f3e26abf271f096ffbeb960
adrian.martinez <adrian.martinez@udea.edu.co>
2017-12-30 09:51:16 -0500

    header modificado

--- vista/header.php
+++ vista/header.php
@@ -4,8 +4,9 @@
 <head>
   <title>lefacturamos</title>
   <meta charset="utf-8"/>
-  <link rel="stylesheet" href="../src/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <link rel="stylesheet" href="../src/css/bootstrap.min.css" >
   <link rel="stylesheet" href="../src/css/estilos.css">
-<script src="../src/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<script src="../src/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body style="bakcground-color:#333333">
